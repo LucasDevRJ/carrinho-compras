ab36befdfcf08d5007a0eeacc56d4655120de21e
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-11-22 18:10:02 -0300

    Adição dos produtos na tela.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,21 @@
 function adicionar() {
     //recuperar valores: nome do produto, quantidade e valor.
-    //calcular o preço, nosso subtotal
-    //adicionar no carrinho
-    //atualizar o valor total
     let produto = document.getElementById("produto").value;
     let nomeProduto = produto.split("-")[0];
     let precoUnitarioProduto = produto.split("R$")[1];
     let quantidadeDoProduto = document.getElementById("quantidade").value;
-    alert(nomeProduto);
-    alert(quantidadeDoProduto);
-    alert(precoUnitarioProduto);
-    let precoTotal = quantidadeDoProduto * precoUnitarioProduto;
-    alert(precoTotal);
+    
+    let precoTotal = document.getElementById("valor-total");
+    //calcular o preço, nosso subtotal
+    let calculoDoPrecoTotal = quantidadeDoProduto * precoUnitarioProduto;
+    //atualizar o valor total
+    precoTotal.innerHTML = `R$${calculoDoPrecoTotal}`;
+    
+    //adicionar no carrinho
+    let carrinho = document.getElementById("lista-produtos");
+    carrinho.innerHTML += `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidadeDoProduto}x </span>${nomeProduto}<span class="texto-azul">R$${precoUnitarioProduto}</span>
+    </section>`
 }
 
 function limpar() {
