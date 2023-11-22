8cff1d0229826334114a911676a2d9ad47f50ebb
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-11-22 18:54:44 -0300

    Função para adicionar produto e limpar carrinho adicionadas

--- js/app.js
+++ js/app.js
@@ -1,23 +1,36 @@
+var acumuladorDePreco;
+
 function adicionar() {
     //recuperar valores: nome do produto, quantidade e valor.
     let produto = document.getElementById("produto").value;
     let nomeProduto = produto.split("-")[0];
     let precoUnitarioProduto = produto.split("R$")[1];
     let quantidadeDoProduto = document.getElementById("quantidade").value;
-    
-    let precoTotal = document.getElementById("valor-total");
-    //calcular o preço, nosso subtotal
-    let calculoDoPrecoTotal = quantidadeDoProduto * precoUnitarioProduto;
-    //atualizar o valor total
-    precoTotal.innerHTML = `R$${calculoDoPrecoTotal}`;
-    
-    //adicionar no carrinho
-    let carrinho = document.getElementById("lista-produtos");
-    carrinho.innerHTML += `<section class="carrinho__produtos__produto">
-    <span class="texto-azul">${quantidadeDoProduto}x </span>${nomeProduto}<span class="texto-azul">R$${precoUnitarioProduto}</span>
-    </section>`
+
+    if (quantidadeDoProduto < 1) {
+        alert("Digite uma quantidade maior ou igual a 1!!");
+    } else {
+        //calcular o preço, nosso subtotal
+        let precoTotal = document.getElementById("valor-total");
+        let calculoDoPrecoTotal = quantidadeDoProduto * precoUnitarioProduto;
+        
+        //adicionar no carrinho
+        let carrinho = document.getElementById("lista-produtos");
+        carrinho.innerHTML += `<section class="carrinho__produtos__produto">
+        <span class="texto-azul">${quantidadeDoProduto}x </span>${nomeProduto}<span class="texto-azul">R$ ${precoUnitarioProduto}</span>
+        </section>`;
+
+        //atualizar o valor total
+        acumuladorDePreco += calculoDoPrecoTotal;
+        precoTotal.innerHTML = `R$${acumuladorDePreco}`;
+        document.getElementById("quantidade").value = 0;
+    }
 }
 
 function limpar() {
-
+    acumuladorDePreco = 0;
+    document.getElementById("lista-produtos").innerHTML = "";
+    document.getElementById("valor-total").innerHTML = `R$ ${acumuladorDePreco}`;
 }
+
+limpar();
