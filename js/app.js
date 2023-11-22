346d16b6e7fa3c10d81a27c898cfb6a08c364480
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-11-22 13:03:54 -0300

    Elementos armazenados no JS.

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function adicionar() {
+    //recuperar valores: nome do produto, quantidade e valor.
+    //calcular o preço, nosso subtotal
+    //adicionar no carrinho
+    //atualizar o valor total
+    let nomeDoProduto = document.getElementById("produto");
+    let quantidadeDoProduto = document.getElementById("quantidade");
+}
+
+function limpar() {
+
+}
